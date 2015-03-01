c573a7cc6c24d83122b5264182bc05152f1e3ea0
Thomas van Steen <tom@vansteen.me>
2015-03-01 22:30:49 +0100

    Update the service provider to work on both Laravel v4 & v5

--- src/Vansteen/Sendinblue/SendinblueServiceProvider.php
+++ src/Vansteen/Sendinblue/SendinblueServiceProvider.php
@@ -20,7 +20,19 @@ class SendinblueServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		$this->package('vansteen/sendinblue');
+		$app = app();
+		$version = $app::VERSION;
+
+		if ($version[0] == 4)
+		{
+			$this->package('vansteen/sendinblue');
+		}
+		elseif ($version[0] == 5)
+		{
+			$this->publishes([
+				__DIR__.'/../../config/config.php' => config_path('sendinblue.php'),
+			]);
+		}
 	}
 
 	/**
@@ -31,8 +43,21 @@ class SendinblueServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		$this->app->singleton('sendinblue_wrapper', function() {
-			$ml = new Mailin('https://api.sendinblue.com/v2.0', Config::get('sendinblue::apikey'));
-			return new SendinblueWrapper($ml);
+
+			$app = app();
+			$version = $app::VERSION;
+
+			if ($version[0] == 4)
+			{
+				$ml = new Mailin('https://api.sendinblue.com/v2.0', Config::get('sendinblue::apikey'));
+				return new SendinblueWrapper($ml);
+			}
+			elseif ($version[0] == 5)
+			{
+				$ml = new Mailin('https://api.sendinblue.com/v2.0', Config::get('sendinblue.apikey'));
+				return new SendinblueWrapper($ml);
+			}
+
 		});
 	}
 
